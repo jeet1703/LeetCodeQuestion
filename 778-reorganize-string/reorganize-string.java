afa080dedba884020b8ae2aa398408f4226ba3cc
jeet1703 <95967255+jeet1703@users.noreply.github.com>
2026-09-11 02:17:16 +0530

    Time: 54 ms (6.60%) | Memory: 52.4 MB (5.23%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    class Pair{
+        char first;
+        int count;
+        public Pair(char first, int count){
+            this.first=first;
+            this.count = count;
+        }
+    }
+    public String reorganizeString(String s) {
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((a,b)->Integer.compare(b.count,a.count));
+        int n = s.length();
+        Map<Character,Integer> mp = new HashMap<>();
+        for(int i =0;i<n;i++){
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        }
+        int index = 0;
+        mp.forEach((key,value)->pq.add(new Pair(key,value)));
+        Pair prev = null;
+        String ans = "";
+        while(!pq.isEmpty()|| prev != null){
+            System.out.println("pq size " + pq.size());
+            if(pq.isEmpty() && prev != null){
+                return "";
+            };
+            Pair temp = pq.poll();
+            ans+=temp.first;
+            System.out.println("ans is " + ans);
+            temp.count = temp.count-1;
+            if(prev != null && prev.count >0){
+                pq.add(prev);
+            }
+            if(temp.count != 0)prev = temp;
+            else prev = null;
+            
+            
+        }
+        System.out.println("here ");
+        return ans;
+    }
+}
